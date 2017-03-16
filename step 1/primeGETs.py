7071bb33960521a981c6b7834ef0622dcd7e6e4e
ivanalu <iwannica88@gmail.com>
2017-03-16 08:58:15 +0100

    Add files via upload

---
+++ step 1/primeGETs.py
@@ -0,0 +1,52 @@
+import requests
+
+apiUrl = "https://198.18.134.53/webacs/api/v1/"
+authToken = "Basic aWx1a2ljOjE4NjUxNw=="
+
+def getAlarms(apiUrl, authToken):
+	apiAlarmUrl = "data/Alarms"
+	headers = {
+    	'authorization': authToken
+  	}
+	response = requests.request("GET", apiUrl + apiAlarmUrl, headers=headers, verify=False)
+	return(response.text)
+
+def getDevices (apiUrl, authToken):
+	apiDevUrl = "data/Devices"
+	headers = {
+    	'authorization': authToken
+  	}
+	response = requests.request("GET", apiUrl + apiDevUrl, headers=headers, verify=False)
+	return(response.text)
+
+def getEvents (apiUrl, authToken):
+	apiEventUrl = "data/Events"
+	headers = {
+    	'authorization': authToken
+    }
+	response = requests.request("GET", apiUrl + apiEventUrl, headers=headers, verify=False)
+	return(response.text)
+
+def getSyslogs (apiUrl, authToken):
+	apiSysUrl = "data/Syslogs"
+	headers = {
+    	'authorization': authToken
+    }
+	response = requests.request("GET", apiUrl + apiSysUrl, headers=headers, verify=False)
+	return(response.text)
+
+def getInventoryDetails (apiUrl, authToken):
+	apiInventUrl = "data/InventoryDetails"
+	headers = {
+    	'authorization': authToken
+    }
+	response = requests.request("GET", apiUrl + apiInventUrl, headers=headers, verify=False)
+	return(response.text)
+
+
+if __name__ == '__main__':
+	print(getAlarms(apiUrl, authToken))
+	print(getDevices(apiUrl, authToken))
+	print(getEvents(apiUrl, authToken))
+	print(getSyslogs(apiUrl, authToken))
+	print(getInventoryDetails(apiUrl, authToken))
