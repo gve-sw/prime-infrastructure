c6c82753a44f4b36f2774bd3cd677c6a36c5d0b6
ivanalu <iwannica88@gmail.com>
2017-03-27 12:37:48 +0200

    Add files via upload

---
+++ step 1/getInventoryDetails.py
@@ -0,0 +1,17 @@
+#Prime Infrastructure
+
+import requests
+
+apiUrl = "https://198.18.134.53/webacs/api/v1/"
+authToken = "Basic aWx1a2ljOjE4OTQ4Ng=="
+
+def getInventoryDetails (apiUrl, authToken):
+	apiInventUrl = "data/InventoryDetails.json"
+	headers = {
+    	'authorization': authToken
+    }
+	response = requests.request("GET", apiUrl + apiInventUrl, headers=headers, verify=False)
+	return(response.text)
+
+if __name__ == '__main__':
+	print(getInventoryDetails(apiUrl, authToken))
